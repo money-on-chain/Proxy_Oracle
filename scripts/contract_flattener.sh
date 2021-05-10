6ea0534425c7e966df8ccaddad2a64c05247b5b5
mulonemartin <mulone.martin@gmail.com>
2021-05-10 15:26:50 -0300

    Eth Mainnet configurations and contract verification

--- scripts/contract_flattener.sh
+++ scripts/contract_flattener.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo "Starting to flatten our contracts"
 node_modules/.bin/truffle-flattener contracts/ProxyMoCMedianizer.sol > scripts/contract_flatten/ProxyMoCMedianizer_flat.sol
+node_modules/.bin/truffle-flattener zos-lib/contracts/upgradeability/AdminUpgradeabilityProxy.sol > scripts/contract_flatten/AdminUpgradeabilityProxy_flat.sol
 echo "Finish successfully! Take a look in folder scripts/contract_flatten/..."
