d43c5978974a2be3b6c1611454dd086d5ee07b58
david weil <david.weil@coinfabrik.com>
2021-12-21 17:56:27 -0300

    working pancake price oracle deploy

--- migrations/x_pancake.js
+++ migrations/x_pancake.js
@@ -1,54 +1,56 @@
 /* eslint-disable no-console */
-//const AdminUpgradeabilityProxy = artifacts.require('AdminUpgradeabilityProxy');
+const AdminUpgradeabilityProxy = artifacts.require('AdminUpgradeabilityProxy');
 const PancakePriceOracle = artifacts.require('./PancakePriceOracle.sol');
+const IPancakeFactory = artifacts.require('./IPancakeFactory.sol');
+const IPancakePair = artifacts.require('./IPancakePair.sol');
 
-const { getConfig, getNetwork, saveConfig } = require('./helper');
+const {getConfig, getNetwork, saveConfig} = require('./helper');
 
 const vtcb = '0x76eDbF5ec4825833479E44A26F7A4D1285295eC7';
 const wbnb = '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd';
 
 
 module.exports = async callback => {
-  try {
-    const network = getNetwork(process.argv);
-    const configPath = `${__dirname}/configs/${network}.json`;
-    const config = getConfig(network, configPath);
+    try {
+        const network = getNetwork(process.argv);
+        const configPath = `${__dirname}/configs/${network}.json`;
+        const config = getConfig(network, configPath);
 
-    // Deploy new ProxyMoCMedianizer implementation
-    //const mocMedianizer = await ProxyMoCMedianizer.new();
-    const pancakePriceOracle = await PancakePriceOracle.new();
+        //const pf = await IPancakeFactory.at(config.pancake_factory);
+        // Deploy new PancakePriceOracle implementation
+        const pancakePriceOracle = await PancakePriceOracle.new();
 
-    // Save implementation address to config file
-    //config.implementationAddresses.PancakePriceOracle = pancakePriceOracle.address;
-    config.PancakePriceOracle = pancakePriceOracle.address;
-    saveConfig(config, configPath);
+        // Save implementation address to config file
+        config.implementationAddresses = config.implementationAddresses || {};
+        config.implementationAddresses.pancakePriceOracle = pancakePriceOracle.address;
+        saveConfig(config, configPath);
 
-    // Initialize contract
-    const initData = await pancakePriceOracle.contract.methods
-      .initialize(
-        config.governor,
-	config.pancake_router,
-	vtcb,
-	wbnb,
-      )
-      .encodeABI();
-    console.log('PancakePriceOracle Initialized');
+	// this one or the following..
+        //await pancakePriceOracle.initialize(config.governor, config.pancake_router, vtcb,wbnb);
+        // Initialize contract
+        const initData = await pancakePriceOracle.contract.methods.initialize(
+                config.governor,
+                config.pancake_router,
+                vtcb,
+                wbnb).encodeABI();
+        console.log('PancakePriceOracle Initialize..');
+        
+        const proxy = await AdminUpgradeabilityProxy.new(
+            pancakePriceOracle.address,
+            config.admin,
+            initData);
+        
+        // Save proxy address to config file
+        config.proxyAddresses = config.proxyAddresses || {};
+        config.proxyAddresses.pancakePriceOracle= proxy.address;
+        saveConfig(config, configPath);
+	    
+        console.log('PancakePriceOracle proxy address: ', proxy.address);
+        console.log('PancakePriceOracle implementation address: ',
+                    pancakePriceOracle.address);
+    } catch (error) {
+        callback(error);
+    }
 
-/*    const proxyMoCMedianizer = await AdminUpgradeabilityProxy.new(
-      pancakePriceOracle.address,
-      config.admin,
-      initData
-    );
-*/
-    // Save proxy address to config file
-//    config.proxyAddresses.ProxyMoCMedianizer = proxyMoCMedianizer.address;
-//    saveConfig(config, configPath);
-
-    //console.log('MoCMedianizer proxy address: ', proxyMoCMedianizer.address);
-    console.log('MoCMedianizer implementation address: ', pancakePriceOracle.address);
-  } catch (error) {
-    callback(error);
-  }
-
-  callback();
+    callback();
 };
